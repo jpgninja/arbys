47c83b55c4836a9d6069d1b70291d9307f33af29
Chris Mewhort <chrismewhort@gmail.com>
2015-09-08 01:47:38 -0400

    Removing more unnecessaries.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var Poloniex = require('poloniex/poloniex.js');
+// var Poloniex = require('poloniex/poloniex.js');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,7 +24,6 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
